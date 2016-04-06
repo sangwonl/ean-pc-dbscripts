cc95193591e4108dbc080a143ee662b380f81408
Jon Arce <jon.arce@gmail.com>
2016-04-06 11:42:34 -0500

    More cleanup
    Clean up data

--- GIATA/EAN_Inventory_Category_Count.sql
+++
@@ -1,7 +0,0 @@
-### Script to extract the amount of hotels per model
-###
-use eanmap;
-SELECT DISTINCT BusinessModelMask, COUNT(BusinessModelMask) AS 'count_per_model',
-                SUM(if(GiataID > 0, 1, 0)) AS 'matched_to_giata'
-FROM ActivePropertyGiata GROUP BY BusinessModelMask
-
--- GIATA/GIATA_Ean_Inventory_list.sql
+++
@@ -1,6 +0,0 @@
-### Script to extract the amount of hotels per model
-###
-use eanprod;
-SELECT DISTINCT BusinessModelMask, COUNT(BusinessModelMask) AS 'count_per_model'
-FROM activepropertybusinessmodel GROUP BY BusinessModelMask
-
