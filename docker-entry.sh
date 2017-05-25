6dd299b53331b36933c0be468ef45be1c39acc92
Sangwon Lee <gamzabaw@gmail.com>
2017-05-25 11:17:17 +0900

    Bugfix cron not working in docker

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+# https://stackoverflow.com/questions/34962020/cron-and-crontab-files-not-executed-in-docker
+# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=647193
+touch /etc/crontab /etc/cron.*/*
+
+export EXTRA_OPTS='-L 15'
+
 service cron start
+
 exec /entrypoint.sh mysqld $@
